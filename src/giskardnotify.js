eb816b177eb86ad564c33c41bd729b9e6f1a8c6e
Victor Gama <hey@vito.io>
2017-03-27 14:54:51 -0300

    Add support to attachments

--- src/giskardnotify.js
+++ src/giskardnotify.js
@@ -6,7 +6,18 @@ var GiskardNotify = function() {
         this.searchChannel(req.params.channel)
             .then((c) => {
                 if(req.body.message) {
-                    c.send(req.body.message);
+                    var attachments;
+                    if(req.body.attachments) {
+                        try {
+                            attachments = JSON.parse(req.body.attachments);
+                        } catch(ex) {
+                            res.send('error: ' + ex.message);
+                            return;
+                        }
+                        c.send(req.body.message, attachments);
+                    } else {
+                        c.send(req.body.message);
+                    }
                     res.send('ok');
                 } else {
                     res.send('Missing message.');
